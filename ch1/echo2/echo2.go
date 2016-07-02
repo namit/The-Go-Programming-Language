e504b3d12e52732023e42720a88ac4f28a301d84
Namit <namityadav@gmail.com>
2016-07-02 01:07:34 -0700

    Echo 2

---
+++ ch1/echo2/echo2.go
@@ -0,0 +1,16 @@
+// Package main provides ...
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
