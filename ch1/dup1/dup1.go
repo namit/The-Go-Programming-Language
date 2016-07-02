b74a004d4a6ca1508a863650d20230adcf197125
Namit <namityadav@gmail.com>
2016-07-02 13:50:49 -0700

    Dup1

---
+++ ch1/dup1/dup1.go
@@ -0,0 +1,19 @@
+// Package main provides ...
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	for line, n := range counts {
+		fmt.Printf("n, line = %d\t%s\n", n, line)
+	}
+}
